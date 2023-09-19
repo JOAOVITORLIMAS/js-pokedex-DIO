c38a9416f4b22ae5d150be81d526874be6f73a5e
JOAOVITORLIMAS <joaovitorlimas@gmail.com>
2023-09-18 22:35:11 -0300

    Paginação dinamica , manipulando HTML e dados da API, ajuste no CSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,51 @@
-
-function convertPokemonTypesToli(pokemonTypes) {
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+const maxRecords = 151
+const limit = 10
+let offset = 0;
 
 function convertPokemonToLi(pokemon) {
-    
-
     return `
-    <li class="pokemon">
-        <span class="number">#${pokemon.order}</span>
+    <li class="pokemon ${pokemon.type}">
+        <span class="number">#${pokemon.number}</span>
         <span class="name">${pokemon.name}</span>
 
         <div class="detail">
-            <ol class="types">
-                ${convertPokemonToLi(pokemon.types).join('')}
-            </ol>
 
-            <img src="${pokemon.sprites.other.dream_world.front_default}"
+             <ol class="types">
+                 ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+             </ol>
+
+            <img src="${pokemon.photo}"
                 alt="${pokemon.name}">
         </div>
-    </li> `
+    </li> 
+    ` 
 }
 
-const pokemonList = document.getElementById('pokemonList')
 
-pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToLi).join('')
+        pokemonList.innerHTML += newHtml
+        })
+    }   
+
+loadPokemonItens(offset, limit)
+
+    loadMoreButton.addEventListener('click', () => {
+    offset += limnit
+
+    const qtdRecordsWithNexPage = offset + limit
+
+    if (qtdRecordsWithNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
 })
 
 
